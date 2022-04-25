19d98f03bb0b7fe030bc6e90f34e4dcda88f560c
YimneRaid <43366463+YimneRaid@users.noreply.github.com>
2022-04-25 23:15:06 +0200

    Sale

--- src/test/java/at/htlleonding/SaleLogicTest.java
+++ src/test/java/at/htlleonding/SaleLogicTest.java
@@ -1,36 +1,42 @@
-//package at.htlleonding;
-//
-//import at.htlleonding.DTOs.SaleDTO;
-//import at.htlleonding.Logic.SaleLogic;
-//import at.htlleonding.persistence.Copy;
-//import at.htlleonding.persistence.Sale;
-//import io.quarkus.test.TestTransaction;
-//import io.quarkus.test.junit.QuarkusTest;
-//import org.junit.jupiter.api.Test;
+package at.htlleonding;
 
-//
-//import javax.inject.Inject;
-//
-//@QuarkusTest
-//public class SaleLogicTest {
-//    @Inject
-//    SaleLogic saleLogic;
-//
-//    @TestTransaction
-//    @Test
-//    /*public void checkIfaGenreCanBeaDuplicate() {
-//        GenreDTO genreDTO1 = new GenreDTO("Action");
-//        GenreDTO genreDTO2 = new GenreDTO("Action");
-//        var genreID = genreLogic.createGenre(genreDTO1);
-//        var genreID2 = genreLogic.createGenre(genreDTO2);
-//
-//        Assertions.assertEquals(genreID, genreID2);
-//
-//        Copy cy1 = new Copy();
-//        Client ct
-//
-//        SaleDTO saleDTO = new SaleDTO(c1.getId(), );
-//
-//        saleLogic.createSale(saleDTO);
-//    }*/
-//}
+import at.htlleonding.DTOs.GenreDTO;
+import at.htlleonding.DTOs.SaleDTO;
+import at.htlleonding.Logic.GenreLogic;
+import at.htlleonding.Logic.LibraryLogic;
+import at.htlleonding.Logic.SaleLogic;
+import at.htlleonding.Logic.TopicLogic;
+import at.htlleonding.persistence.Client;
+import at.htlleonding.persistence.Copy;
+import at.htlleonding.persistence.Genre;
+import io.quarkus.test.TestTransaction;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+@QuarkusTest
+public class SaleLogicTest {
+    @Inject
+    SaleLogic saleLogic;
+
+    @TestTransaction
+    @Test
+    public void checkIfaGenreCanBeaDuplicate() {
+        GenreDTO genreDTO1 = new GenreDTO("Action");
+        GenreDTO genreDTO2 = new GenreDTO("Action");
+        var genreID = GenreLogic.createGenre(genreDTO1);
+        var genreID2 = GenreLogic.createGenre(genreDTO2);
+
+        Assertions.assertEquals(genreID, genreID2);
+
+        Copy cy1 = new Copy();
+        Client ct
+
+        SaleDTO saleDTO = new SaleDTO(c1.getId(), );
+
+        saleLogic.createSale(saleDTO);
+    }
+}
